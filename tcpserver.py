8b7728285b0284a933093401620e407215939405
thazinnaing <119584255+thazinnaing@users.noreply.github.com>
2023-02-26 17:48:45 +0630

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -5,10 +5,9 @@ from deliverypj import delivery
 
 
 class TCP_server():
-
     connection = MongoClient("localhost", 27017)
     database = connection["DeliveryDB"]
-    collection = database["foodcollection"]
+    collection = database["usercollection"]
 
     def __init__(self):
         self.server_ip = 'localhost'
@@ -28,18 +27,37 @@ class TCP_server():
     def handle_client(self, client_socket):
         with client_socket as sock:
             request = sock.recv(1024).decode("utf-8")
-            inidata = int(request)
-            obj = delivery()
-            if inidata == 1:
-
-                return_data = obj.showMenu()
-                sock.send(return_data.encode())
-
-            if inidata == 2:
-                return_data = obj.create_account()
-                sock.send(return_data.encode())
+            print("receive data from client: ", request)
 
 
+            if request == '1':
+                self.show(sock)
+
+            elif request == '2':
+                self.create(sock)
+
+
+    def show(self, sock):
+
+        obj = delivery()
+        return_data = obj.showMenu()
+        sock.send(return_data.encode())
+
+
+    def create(self, sock):
+
+        obj = delivery()
+        return_data = obj.create_account()
+        sock.send(return_data.encode())
+        recvdata = sock.recv(4096).decode("utf-8")
+        print("receive string: ", recvdata)
+
+        confirmdata = obj.confirmpassword(recvdata)
+        if confirmdata == 1:
+            obj.store_account(recvdata)
+        else:
+            print("Incorrect password")
+            self.create(sock)
 
 
 if __name__ == '__main__':
