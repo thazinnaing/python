7f3f70c562ddbf19819b57f7ca741d8c40720115
thazinnaing <119584255+thazinnaing@users.noreply.github.com>
2023-03-04 16:13:28 +0630

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -37,7 +37,9 @@ class Client:
         print(">>>>>>>>>>>>This is our menu<<<<<<<<<<<<\n", recvFromServer)
         data = "Done"
         self.client.send(data.encode())
-        self.order()
+        self.chooseoption()
+
+
 
     def create(self, recvFromServer):
         obj = delivery()
@@ -54,7 +56,7 @@ class Client:
     def login(self, recvFromServer):
         obj = delivery()
         fdata = obj.splitdata("*", recvFromServer)
-        self.client.send(fdata)
+        self.client.send(fdata.encode())
         wrongdata = self.client.recv(4096).decode("utf-8")
 
         if recvFromServer == wrongdata:
@@ -62,9 +64,41 @@ class Client:
         else:
             print("receive from server data", wrongdata)
 
-    def order(self):
+    def chooseoption(self):
         recvFromServer = self.client.recv(4096).decode("utf-8")
         data = input(recvFromServer)
+        self.client.send(data.encode())
+        recv = self.client.recv(4096).decode("utf-8")
+        if "food" in recv:
+            self.choose_order(recv)
+
+        elif "cancel" in recv:
+            self.cancelitem(recv)
+
+        elif "amount" in recv:
+            paymethod = input(recv)
+            self.client.send(paymethod.encode())
+
+    def cancelitem(self, recv):
+        canceli = input(recv)
+        self.client.send(canceli.encode())
+        rece = self.client.recv(4096).decode("utf-8")
+        print(rece)
+        self.chooseoption()
+
+    def choose_order(self, recv):
+        foodordrink = input(recv)
+        self.client.send(foodordrink.encode())
+        r = self.client.recv(4096).decode("utf-8")
+        re = input(r)
+        self.client.send(re.encode())
+        rece = self.client.recv(4096).decode("utf-8")
+        receive = input(rece)
+        self.client.send(receive.encode())
+        rr = self.client.recv(4096).decode("utf-8")
+        toinput = input(rr)
+        self.client.send(toinput.encode())
+        self.chooseoption()
 
 
 if __name__ == "__main__":
